dedccca2fbb317e28e1f07a3851d1d02c9aadfb2
Wilbert J Chagula <wjchagula@gmail.com>
2019-04-09 15:55:16 -0400

    removed a file

--- src/Controllers/project_router.js
+++ src/Controllers/project_router.js
@@ -7,3 +7,8 @@ app.get('/' , (req, res)  =>{
     var list = [1,2,3];
     res.json(list)
 });
+
+const port = provess.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
