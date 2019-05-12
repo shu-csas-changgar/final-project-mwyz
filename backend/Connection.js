3cc78f74aaa38673f5dc381429bb8efb5a9d773b
Yohan Ninan <yohanninan@gmail.com>
2019-05-11 20:12:05 -0400

    Add files via upload

--- backend/Connection.js
+++ backend/Connection.js
@@ -1,36 +1,37 @@
-class SQLConnection{
-  constructor(user="root", password="password", database='abc', host="localhost") {
-      var mysql = require('mysql');
-      this.database = database;
-      this.con = mysql.createConnection({
-      host: host,
-      user: user,
-      password: password,
-      database: database
-      });
-  }
-
-  getQuery(query){
-    this.con.connect(function(err) {
-      if (err) throw err;
-      con.query(query, function (err, result) {
-        if (err) throw err;
-        return result;
-      });
-    });
-  }
-
-  runQuery(query){
-    this.con.connect(function(err) {
-      if (err) throw err;
-      con.query(query, function (err, result) {
-        if (err) throw err;
-        console.log("table updated");
-      });
-    });
-  }
+const mysql = require( 'mysql' );
+class SQLConnection {
+    constructor(user="root", password="password", database='abc', host="localhost") {
+        this.database = database;
+        this.con = mysql.createConnection({
+        host: host,
+        user: user,
+        password: password,
+        database: database
+        });
+    }
+    query(sql, args) {
+        return new Promise( ( resolve, reject ) => {
+            this.con.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.con.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
 }
 
 const Query = require('./SQLQuery.js');
 let Q = new Query();
+
+var c = new SQLConnection();
+console.log(c.query(Q.select("*", "city")));
 console.log(Q.select("*", 'city'));
